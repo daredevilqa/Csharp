a584eccd727296545125c4a5bad76b3262090a40
Maxim Zakharov <ekb.maxi@gmail.com>
2020-07-05 11:47:21 +0500

    Solution for checking time zones codes and info

---
+++ TimeZone/TimeZone/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TimeZone
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Current local time zone code: '{TimeZoneInfo.Local.Id}'");
+            Console.WriteLine($"UTC time zone code: '{TimeZoneInfo.Utc.Id}'");
+            Console.WriteLine();
+            Console.WriteLine("System has the following info about time zones:");
+
+            foreach (var tz in TimeZoneInfo.GetSystemTimeZones()) {
+                Console.WriteLine($"{tz} has '{tz.Id}' ID");
+            }
+            Console.ReadLine();
+        }
+    }
+}
