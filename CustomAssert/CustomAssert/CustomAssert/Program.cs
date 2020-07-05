7f283055c2228671261eebd9d0c6065418353cac
Maxim Zakharov <ekb.maxi@gmail.com>
2020-07-05 11:43:49 +0500

    solution for quick assertions checks

---
+++ CustomAssert/CustomAssert/CustomAssert/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CustomAssert
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var expected = "Don't Worry";
+            Console.WriteLine($"Let's try it out. Type in: {expected}");
+            var actual = Console.ReadLine();
+            StAssert.AreEqual(expected, actual, "Seems like you made a typo. Try again");
+        }
+    }
+
+    public static class StAssert
+    {
+        public static void AreEqual(string expected, string actual, string msg)
+        {
+            try {
+                Assert.AreEqual(expected, actual, msg);
+                //StringAssert.AreEqualIgnoringCase(expected, actual, msg);
+            }
+            catch (AssertionException e) {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("RESULT");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Exception message:");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Stack Trace:");
+                Console.WriteLine(e.StackTrace);
+                throw e;
+            }
+        }
+    }
+}
