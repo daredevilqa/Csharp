2855a7968080c78f2a70079c62bb8b8792b5899d
Maxim Zakharov <ekb.maxi@gmail.com>
2020-10-04 00:23:13 +0500

    : initial commit

---
+++ W3Schools_TrySQL_Tests/W3Schools_TrySQL_Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace W3Schools_TrySQL_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
